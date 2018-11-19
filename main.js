5a2c2dc417a57f54f2691ea02973e96dc4619ce6
alfmat <alfredmathew@outlook.com>
2018-11-19 18:10:38 -0500

    added comments, intro new methods in main.js

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+/**
+ * This function changes the text of a paragraph upon
+ * being called
+ */
 function myFunction() {
     document.getElementById("introductory_text").innerHTML = "now changed";
+}
+
+/**
+ * This function is meant to display a picture to the user
+ * upon being called
+ */
+function displayPic() {
+    document.write()
+}
+
+function displayDate() {
+    return new Date();
+}
+
+/**
+ * Converts an array to a list of string type
+ * @param {*} array 
+ */
+function convertStringArray (array) {
+    return array.toString();
 }
